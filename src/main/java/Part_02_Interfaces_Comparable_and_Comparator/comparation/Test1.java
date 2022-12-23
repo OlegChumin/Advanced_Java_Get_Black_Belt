d626c6d2f8122dbe25b705285a04afc3ae2823e1
Oleg Chumin <tschumin.oleg@gmail.com>
2022-12-23 11:18:49 +0300

    Add new files

---
+++ src/main/java/Part_02_Interfaces_Comparable_and_Comparator/comparation/Test1.java
@@ -0,0 +1,19 @@
+package Part_02_Interfaces_Comparable_and_Comparator.comparation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test1 {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        list.add("Murzik");
+        list.add("Lotar");
+        list.add("Erna");
+        list.add("Sherlock");
+        list.add("Vasia");
+        list.add("Ryzik");
+        list.add("Uiliam");
+        list.add("Katana");
+        list.add("Python");
+    }
+}
