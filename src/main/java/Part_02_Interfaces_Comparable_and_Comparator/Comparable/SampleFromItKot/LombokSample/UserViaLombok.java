8c89e2d4b5554b1b5597b5b08c2b74c5c79d8a6e
Oleg Chumin <tschumin.oleg@gmail.com>
2024-05-31 16:04:49 +0300

    Add UserViaLombok

---
+++ src/main/java/Part_02_Interfaces_Comparable_and_Comparator/Comparable/SampleFromItKot/LombokSample/UserViaLombok.java
@@ -0,0 +1,36 @@
+package Part_02_Interfaces_Comparable_and_Comparator.Comparable.SampleFromItKot.LombokSample;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+/**
+ * @NoArgsConstructor
+ * Генерирует конструктор без аргументов.
+ *
+ * @RequiredArgsConstructor
+ * Генерирует конструктор для полей, помеченных как final, и полей с аннотацией @NonNull.
+ *
+ * @AllArgsConstructor
+ * Генерирует конструктор со всеми полями класса.
+ *
+ * @Builder
+ * Позволяет создавать объекты класса с использованием паттерна "Builder".
+ * та аннотация не только генерирует конструктор, но и предоставляет удобный API для построения объектов.
+ */
+
+@Getter
+@ToString
+@AllArgsConstructor
+public class UserViaLombok implements Comparable<UserViaLombok> {
+    private String name;
+    private int age;
+    private int salary;
+
+    @Override
+    public int compareTo(UserViaLombok other) {
+        // Реализация метода сравнения
+        return Integer.compare(this.salary, other.salary);
+    }
+}
